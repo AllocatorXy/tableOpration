939e88e3652029d8693bf9c1ea0a6edbcd8e631d
AllocatorXy <misaka20001@vip.qq.com>
2016-09-09 01:57:03 +0800

    annotation

--- js/js.js
+++ js/js.js
@@ -3,7 +3,7 @@ window.onload = function ()
 	var oTab = document.getElementById('tab1');
 	var exColor = '';
 
-	for (var i = 0; i < oTab.tBodies[0].rows.length; i++) 
+	for (var i = 0; i < oTab.tBodies[0].rows.length; i++) //鼠标划过高亮
 	{
 		oTab.tBodies[0].rows[i].onmouseover = function () 
 			{
