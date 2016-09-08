de9654611634bf19d832c09b1cf4bad6b0e49e73
AllocatorXy <misaka20001@vip.qq.com>
2016-09-09 01:55:28 +0800

    update the colorChange function when hover

---
+++ js/js.js
@@ -0,0 +1,18 @@
+window.onload = function () 
+{
+	var oTab = document.getElementById('tab1');
+	var exColor = '';
+
+	for (var i = 0; i < oTab.tBodies[0].rows.length; i++) 
+	{
+		oTab.tBodies[0].rows[i].onmouseover = function () 
+			{
+				exColor = this.style.backgroundColor;
+				this.style.backgroundColor = 'yellow';
+			};
+		oTab.tBodies[0].rows[i].onmouseout = function () 
+			{
+				this.style.backgroundColor = exColor;
+			};
+	}
+};
