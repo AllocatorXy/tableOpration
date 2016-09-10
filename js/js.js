39f45e1f5d85d037c45813269adf3361efbb37e6
AllocatorXy <misaka20001@vip.qq.com>
2016-09-10 11:34:37 +0800

    debug

--- js/js.js
+++ js/js.js
@@ -4,6 +4,7 @@ window.onload = function ()
 	var exColor = '';
 	var oBtnInsert = document.getElementById('btn1');
 	var oBtnSearch = document.getElementById('btn2');
+	var oBtnFilter = document.getElementById('btn3');
 	var id = oTab.tBodies[0].rows.length+1;
 	var oName = document.getElementById('name');
 	var oAge = document.getElementById('age');
@@ -146,16 +147,21 @@ window.onload = function ()
 		oTab.tBodies[0].appendChild(oTr); //插入到tbodies
 	};
 
-	/*模糊匹配*/
+	/*模糊搜索*/
 	oBtnSearch.onclick = function () 
 	{
-		var flag = 0;
-		var sTxt = oSearch.value.toLowerCase(); 
-		if (checkSpace(sTxt,defValSc) === true) 
+		/*若输入内容非法，则重置列表并跳出fun*/
+		if (checkSpace(oSearch.value,defValSc) === true) 
 		{
-		 	alert('请输入搜索内容');
+		 	alert('请输入搜索内容！');
+		 	for (var k = 0; k < oTab.tBodies[0].rows.length; k++) 
+		 	{
+		 		oTab.tBodies[0].rows[k].style.backgroundColor = '#f2f7fc';
+		 	}
 		 	return false;
 		} 
+		var flag = 0;
+		var sTxt = oSearch.value.toLowerCase(); 
 		var arr = sTxt.split(' ');
 		for (var i = 0; i < oTab.tBodies[0].rows.length; i++) 
 		{
@@ -163,8 +169,9 @@ window.onload = function ()
 			oTab.tBodies[0].rows[i].style.backgroundColor = '#f2f7fc'; //首先把背景色重置
 			for (var j = 0; j < arr.length; j++) //遍历查找多个关键字是否匹配
 			{
-				if (sTab.search(arr[j])!=-1) //哪个匹配就高亮哪行
+				if (sTab.search(arr[j])!=-1 && arr[j]!=='') //匹配到空串以外的东西
 				{
+					// alert(sTab.search(arr[j]));
 					oTab.tBodies[0].rows[i].style.backgroundColor = 'yellow';
 					flag++;
 				}
@@ -176,4 +183,31 @@ window.onload = function ()
 		}
 	};
 
+	/*筛选*/
+	oBtnFilter.onclick = function () 
+	{
+		/*若输入内容非法，则重置列表并跳出fun*/
+		if (checkSpace(oSearch.value, defValSc) === true) 
+		{
+		 	for (var k = 0; k < oTab.tBodies[0].rows.length; k++) 
+		 	{
+		 		oTab.tBodies[0].rows[k].style.display = '';
+		 	}
+		 	return 0;
+		} 
+		var sTxt = oSearch.value.toLowerCase(); 
+		var arr = sTxt.split(' ');
+		for (var i = 0; i < oTab.tBodies[0].rows.length; i++) 
+		{
+			var sTab = oTab.tBodies[0].rows[i].cells[1].innerHTML.toLowerCase();
+			oTab.tBodies[0].rows[i].style.display = 'none';
+			for (var j = 0; j < arr.length; j++) //遍历查找多个关键字是否匹配
+			{
+				if (sTab.search(arr[j])!=-1 && arr[j]!=='') //匹配到空以外的
+				{
+					oTab.tBodies[0].rows[i].style.display = '';
+				}
+			}
+		}
+	};
 };
